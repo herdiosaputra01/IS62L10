2572d0d5f187698ee35856329f8cfc68b3e920a6
herdiosaputra01 <herdiosaputra06@gmail.com>
2025-02-21 10:38:47 +0700

    route.part1

---
+++ resources/views/contoh.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documaent</title>
+</head>
+<body>
+    Web kita-kita aja
+</body>
+</html>
