ff67990871ffebed277788278f3ab4e7e60ccaaf
herdiosaputra01 <herdiosaputra06@gmail.com>
2025-03-14 10:44:46 +0700

    layout

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -66,6 +66,20 @@
     @empty
         <div class="alert alert-secondary d-inline-block">Data Nilai Tidak Ada</div>   
     @endforelse
+            <hr>
+
+    {{-- break continue--}}
+            @forelse ($nilai2 as $val2)
+            @if ($val2 <= 50)
+                @continue
+            @endif
+            <div class="alert alert-success d-inline-block">
+                {{$val2}}
+            </div>
+            @empty
+            <div class="alert alert-secondary d-inline-block">Data Nilai Tidak Ada</div>   
+            @endforelse
+
 
     </div>
     <hr>
